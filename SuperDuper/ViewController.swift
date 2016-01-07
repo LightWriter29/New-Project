2cf09c59874ee26b90f41d312995241cfabbab63
Steven Elliot <StevenElliot@Stevens-MacBook-Pro.local>
2016-01-06 16:04:33 -0800

    new stuff

--- SuperDuper/ViewController.swift
+++ SuperDuper/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Side: UIImageView!
+    @IBOutlet weak var beaver13: UIImageView!
+    @IBOutlet weak var Button1: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +21,12 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func doButtonStuff(sender: AnyObject) {
+        Side.hidden = false
+        beaver13.hidden = false
+        Button1.hidden = true
+        
     }
 
 
